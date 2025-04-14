2308fa4eef23ac2b782823f98f805da0db967eac
SidJM <89504263+SidJM@users.noreply.github.com>
2025-04-14 10:38:06 -0300

    Add files via upload

--- ParametrosInvalidosException.java
+++ ParametrosInvalidosException.java
@@ -1,13 +1,7 @@
 package Exception;
 
-import java.io.Serial;
 
 public class ParametrosInvalidosException extends Exception {
-    /**
-     *
-     */
-    @Serial
-    private static final long serialVersionUID = 1L;
 
     public ParametrosInvalidosException() {
         System.out.println("O segundo parâmetro deve ser maior que o primeiro");
